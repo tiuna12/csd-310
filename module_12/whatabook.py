1a1b5d2866fd681525481eee34aec2712f2d38b1
tiuna12 <125109246+tiuna12@users.noreply.github.com>
2023-08-13 04:17:11 -0400

    update

--- module_12/whatabook.py
+++ module_12/whatabook.py
@@ -1,5 +1,6 @@
 # IMPORT STATEMENTS
 import sys
+from mysql import connector
 import mysql.connector
 from mysql.connector import errorcode
 
@@ -67,7 +68,19 @@ def show_books_to_add(cursor, user_id):
     for book in available_books:
         print(f"ID: {book[0]}, Name: {book[1]}, Author: {book[2]}, Details: {book[3]}")
 
-}
+def show_account_menu():
+
+    try:
+        print("\n      -- Customer Menu --")
+        print("        1. Wishlist\n        2. Add Book\n        3. Main Menu")
+        account_option = int(input('        <Example enter: 1 for wishlist>: '))
+
+        return account_option
+    except ValueError:
+        print("\n  Invalid number, program terminated...\n")
+
+        sys.exit(0)
+
 def main():
     intro()
     
@@ -82,13 +95,13 @@ def main():
         elif choice == 3:
             user_id = validate_user()
             if user_id:
-                show_account_menu( user_id)
+                show_account_menu()
         elif choice == 4:
             break
         else:
             print("Invalid choice. Please select a valid option.")
 
-    conn.close()
+    connector.close()
 
 if __name__ == "__main__":
     main()
